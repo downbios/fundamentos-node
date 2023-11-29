d8e8e875c42a6b39b368a660415af077a8d7e022
ian adson <ian.adson.vieira@gmail.com>
2023-11-28 21:09:41 -0300

    salvando dados stateful

--- src/server.js
+++ src/server.js
@@ -1,7 +1,25 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) =>{
- return res.end('hello word')
+    const {method, url} = req
+
+    if (method === 'GET' && url === '/users'){
+        return res
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method === 'POST' && url === '/users'){
+        users.push({
+            id: 1,
+            name: 'ian Adson',
+            email: 'ian.adson@example.com.br',
+        })
+        return res.end('criação de usuário')
+    }
+        return res.end('hello word')
 })
 
 server.listen(3000)
